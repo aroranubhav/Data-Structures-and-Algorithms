350449ceff0af5fd6b3531712dd1b45b2af406a2
maxi <arora.anubhav93@gmail.com>
2025-04-14 17:55:45 +0530

    Add solutions for problems 153 and 33

---
+++ Binary Search/Find Minimum In Rotated Sorted Array/Find Minimum In Rotated Sorted Array.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun findMinimum(nums: IntArray): Int {
+        var left = 0
+        var right = nums.size - 1
+
+        while (left < right) {
+            var mid = (left + right) / 2
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1
+            } else {
+                right = mid
+            }
+        }
+        return nums[left]
+    }
+}
+
+//Time Complexity: O(log n)
+//Space Complexity: O(1)
